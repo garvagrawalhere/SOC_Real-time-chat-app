6fb7cbbffabcd43e98cedb781095ec883a167d9c
garvagrawalhere <atcvns9@gmail.com>
2026-07-09 19:32:36 +0530

    env added

--- react-app/src/firebase.js
+++ react-app/src/firebase.js
@@ -8,12 +8,13 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAwaShR4WBGU-WIcBdA0YxTaJqraJpY9SI",
-  authDomain: "chat-app-ab3aa.firebaseapp.com",
-  projectId: "chat-app-ab3aa",
-  storageBucket: "chat-app-ab3aa.firebasestorage.app",
-  messagingSenderId: "266940220162",
-  appId: "1:266940220162:web:d43176974ab77372ff781b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 };
 
 // Initialize Firebase
